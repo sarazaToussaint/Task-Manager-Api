49964d74e36500af64000f43a561e0d4121c57f6
sarazaToussaint <tmsaraza@gmail.com>
2023-06-22 18:14:51 +0200

    Setup the project with initial files

---
+++ starter/controllers/tasks.js
@@ -0,0 +1,28 @@
+
+const getAllTask = (req, res) => {
+    res.send('Get all task');
+}
+
+const createTask = (req, res) => {
+  res.json(req.body);
+}
+
+const getTask = (req, res) => {
+  res.json({id: req.params.id });
+}
+
+const updateTask = (req, res) => {
+  res.send('Update task');
+}
+
+const deleteTask = (req, res) => {
+  res.send('Delete task');
+}
+
+module.exports = {
+    getAllTask,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
