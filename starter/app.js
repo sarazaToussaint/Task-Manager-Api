20b67d09f587d52d9e32597c204272d47f0289e6
sarazaToussaint <tmsaraza@gmail.com>
2023-06-24 23:16:01 +0200

    Make a change in a App.js file

--- starter/app.js
+++ starter/app.js
@@ -3,9 +3,11 @@ const app = express();
 const tasks = require('./routes/tasks');
 
 // middleware
+
 app.use(express.json());
 
 // routes
+
 app.get('/hello', (req, res) => {
    res.send('<h1>Task Manager App</h1>'); 
 });
