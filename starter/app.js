1060379d19c15586abde46e0632eb3e59380f436
sarazaToussaint <tmsaraza@gmail.com>
2023-06-26 17:49:19 +0200

    Add crud operation functions

--- starter/app.js
+++ starter/app.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
-
 app.use(express.json());
 
 // routes
-
 app.get('/hello', (req, res) => {
-   res.send('<h2>Task Manager App</h2>'); 
+   res.send('<h2>Task Manager</h2>'); 
 });
 
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}... `);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}... `));
+  }catch (err){
+    console.log(err);
+  } 
+
+  }
+  
+
+start();
